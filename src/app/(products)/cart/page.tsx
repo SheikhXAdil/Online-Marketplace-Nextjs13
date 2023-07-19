4a405119f94cab3a7f58b72be70cd4d851968676
Adil Nadeem <adilnadeem709@gmail.com>
2023-07-20 00:08:11 +0500

    updated domain

--- src/app/(products)/cart/page.tsx
+++ src/app/(products)/cart/page.tsx
@@ -9,7 +9,7 @@ const checkEnvironment = () => {
     let base_url =
         process.env.VERCEL_ENV === "development"
             ? "http://localhost:3000"
-            : "https://online-marketplace-adil.vercel.app";
+            : "https://mydinemart.vercel.app";
     return base_url;
 };
 
